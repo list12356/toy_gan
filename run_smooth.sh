deda5d68e881710e42c3e43b4740f7386d3690b8
list12356 <list12356@sina.com>
2018-04-23 22:25:17 -0500

    Add scripts

--- run_smooth.sh
+++ run_smooth.sh
@@ -1,12 +1,12 @@
-python3 brs_gan_smooth.py --dir="mnist_alpha_1_loss1000_noreg" --alpha=1 --l=1000 &
-python3 brs_gan_smooth.py --dir="mnist_alpha_1_loss100_noreg" --alpha=1 --l=100 &
-python3 brs_gan_smooth.py --dir="mnist_alpha_1_loss10_noreg" --alpha=1 --l=10 &
-python3 brs_gan_smooth.py --dir="mnist_alpha_1_loss1_noreg" --alpha=1 --l=1 &
-python3 brs_gan_smooth.py --dir="mnist_alpha_0.1_loss1000_noreg" --alpha=0.1 --l=1000 &
-python3 brs_gan_smooth.py --dir="mnist_alpha_0.1_loss100_noreg" --alpha=0.1 --l=100 &
-python3 brs_gan_smooth.py --dir="mnist_alpha_0.1_loss10_noreg" --alpha=0.1 --l=10 &
-python3 brs_gan_smooth.py --dir="mnist_alpha_0.1_loss1_noreg" --alpha=0.1 --l=1 &
-python3 brs_gan_smooth.py --dir="mnist_alpha_0.01_loss1000_noreg" --alpha=0.01 --l=1000 &
-python3 brs_gan_smooth.py --dir="mnist_alpha_0.01_loss100_noreg" --alpha=0.01 --l=100 &
-python3 brs_gan_smooth.py --dir="mnist_alpha_0.01_loss10_noreg" --alpha=0.01 --l=10 &
-python3 brs_gan_smooth.py --dir="mnist_alpha_0.01_loss1_noreg" --alpha=0.01 --l=1
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_1_loss1000_noreg" --alpha=1 --l=1000 &
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_1_loss100_noreg" --alpha=1 --l=100 &
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_1_loss10_noreg" --alpha=1 --l=10 &
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_1_loss1_noreg" --alpha=1 --l=1 &
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_0.1_loss1000_noreg" --alpha=0.1 --l=1000 &
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_0.1_loss100_noreg" --alpha=0.1 --l=100 &
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_0.1_loss10_noreg" --alpha=0.1 --l=10 &
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_0.1_loss1_noreg" --alpha=0.1 --l=1 &
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_0.01_loss1000_noreg" --alpha=0.01 --l=1000 &
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_0.01_loss100_noreg" --alpha=0.01 --l=100 &
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_0.01_loss10_noreg" --alpha=0.01 --l=10 &
+python3 brs_gan_smooth.py --dir="mnist_smooth_alpha_0.01_loss1_noreg" --alpha=0.01 --l=1
